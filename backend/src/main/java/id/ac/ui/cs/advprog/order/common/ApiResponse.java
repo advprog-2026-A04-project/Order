fb497eddacf6e442e07b9564d70cd576ec255f05
anitakny <antkhrnns9@gmail.com>
2026-03-05 14:24:08 +0700

    Add common response dan Error handling

---
+++ backend/src/main/java/id/ac/ui/cs/advprog/order/common/ApiResponse.java
@@ -0,0 +1,11 @@
+package id.ac.ui.cs.advprog.order.common;
+
+public record ApiResponse<T>(boolean success, T data, ApiError error) {
+public static <T> ApiResponse<T> ok(T data) {
+    return new ApiResponse<>(true, data, null);
+}
+
+public static <T> ApiResponse<T> fail(ApiError error) {
+    return new ApiResponse<>(false, null, error);
+}
+}
